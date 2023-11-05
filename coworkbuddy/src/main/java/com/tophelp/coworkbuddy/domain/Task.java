6a7567249e88621e6a1bc165275b521da34b471a
Jose Doval <j.doval.fraga@gmail.com>
2023-11-05 14:05:07 +0100

    Add Task and Worker entities with relationships to Room entity
    - Created Task entity with fields and annotations.
    - Created Worker entity with fields and annotations.
    - Defined one-to-many relationship between Room and Task entities.
    - Defined many-to-one relationship between Worker and Room entities.

--- coworkbuddy/src/main/java/com/tophelp/coworkbuddy/domain/Task.java
+++ coworkbuddy/src/main/java/com/tophelp/coworkbuddy/domain/Task.java
@@ -5,33 +5,28 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Set;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
-@Entity(name = "rooms")
+@Entity(name = "tasks")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Room {
+public class Task {
 
     @Id
     private UUID id;
     @Column(nullable = false)
     private String name;
+    private boolean active;
     @ManyToOne
-    @JoinColumn(name="user_id")
-    private User user;
-    @OneToMany(mappedBy = "room")
-    private Set<Task> tasks;
-    @OneToMany(mappedBy = "room")
-    private Set<Worker> workers;
+    @JoinColumn(name = "room_id")
+    private Room room;
 }
